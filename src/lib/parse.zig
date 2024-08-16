61ce8888f565a73f7ded6ae2c70c19e041592106
pomi601 <pomi601@aucklanduni.ac.nz>
2024-08-16 16:14:53 +1200

    wip

--- src/lib/parse.zig
+++ src/lib/parse.zig
@@ -1,5 +1,6 @@
 //! Inspired by Zig tokenizer.
 const std = @import("std");
+const testing = std.testing;
 
 pub const Token = struct {
     tag: Tag,
@@ -14,8 +15,8 @@ pub const Token = struct {
         invalid,
         identifier,
         string_literal,
-        version_literal,
         colon,
+        comma,
         end_field,
         end_stanza,
         open_round,
@@ -27,6 +28,29 @@ pub const Token = struct {
         greater_than,
         eof,
     };
+    pub fn lexeme(token: Token, source: []const u8) ?[]const u8 {
+        return switch (token.tag) {
+            .end_field,
+            .end_stanza,
+            .eof,
+            .invalid,
+            => null,
+
+            .identifier,
+            .string_literal,
+            .less_than,
+            .less_than_equal,
+            .equal,
+            .greater_than_equal,
+            .greater_than,
+            => source[token.loc.start..token.loc.end],
+
+            .colon => ":",
+            .comma => ",",
+            .open_round => "(",
+            .close_round => ")",
+        };
+    }
 };
 
 pub const Tokenizer = struct {
@@ -44,21 +68,30 @@ pub const Tokenizer = struct {
 
     const State = enum {
         start,
+        identifier,
         string_literal,
         string_literal_backslash,
         version_literal,
         version_literal_dot,
+        version_literal_r,
+        version_literal_r_digit,
+        expect_version,
         newline,
+        open_angle,
+        open_angle_equal,
+        close_angle,
+        close_angle_equal,
+        equal,
         invalid,
     };
 
     pub fn next(self: *Tokenizer) Token {
         var state: State = .start;
         var result: Token = .{
-            .tag = undefined,
+            .tag = .eof,
             .loc = .{
                 .start = self.index,
-                .end = undefined,
+                .end = self.index,
             },
         };
 
@@ -85,7 +118,6 @@ pub const Tokenizer = struct {
                     },
                     '0'...'9' => {
                         state = .version_literal;
-                        result.tag = .version_literal;
                     },
                     '"' => {
                         state = .string_literal;
@@ -93,14 +125,146 @@ pub const Tokenizer = struct {
                     },
                     '(' => {
                         result.tag = .open_round;
-                        result.index += 1;
+                        self.index += 1;
                         break;
                     },
                     ')' => {
                         result.tag = .close_round;
-                        result.index += 1;
+                        self.index += 1;
                         break;
                     },
+                    '<' => {
+                        state = .open_angle;
+                    },
+                    '>' => {
+                        state = .close_angle;
+                    },
+                    '=' => {
+                        state = .equal;
+                    },
+                    ':' => {
+                        result.tag = .colon;
+                        self.index += 1;
+                        break;
+                    },
+                    ',' => {
+                        result.tag = .comma;
+                        self.index += 1;
+                        break;
+                    },
+
+                    else => {
+                        state = .invalid;
+                    },
+                },
+                .open_angle => {
+                    switch (c) {
+                        ' ', '\r', '\n', '\t' => {
+                            state = .expect_version;
+                            result.tag = .less_than;
+                        },
+                        '=' => {
+                            state = .expect_version;
+                            result.tag = .less_than_equal;
+                        },
+                        'r', '0'...'9' => {
+                            state = .expect_version;
+                            result.tag = .less_than;
+                            self.index -= 1; // backtrack
+                        },
+                        else => {
+                            state = .invalid;
+                        },
+                    }
+                },
+                .open_angle_equal => {
+                    switch (c) {
+                        ' ', '\r', '\n', '\t' => {
+                            state = .expect_version;
+                            result.tag = .less_than_equal;
+                        },
+                        'r', '0'...'9' => {
+                            state = .expect_version;
+                            result.tag = .less_than_equal;
+                            self.index -= 1; // backtrack
+                        },
+                        else => {
+                            state = .invalid;
+                        },
+                    }
+                },
+                .close_angle => {
+                    switch (c) {
+                        ' ', '\r', '\n', '\t' => {
+                            state = .expect_version;
+                            result.tag = .greater_than;
+                        },
+                        '=' => {
+                            state = .expect_version;
+                            result.tag = .greater_than_equal;
+                        },
+                        'r', '0'...'9' => {
+                            state = .expect_version;
+                            result.tag = .greater_than;
+                            self.index -= 1; // backtrack
+                        },
+                        else => {
+                            state = .invalid;
+                        },
+                    }
+                },
+                .close_angle_equal => {
+                    switch (c) {
+                        ' ', '\r', '\n', '\t' => {
+                            state = .expect_version;
+                            result.tag = .greater_than_equal;
+                        },
+                        'r', '0'...'9' => {
+                            state = .expect_version;
+                            result.tag = .greater_than_equal;
+                            self.index -= 1; // backtrack
+                        },
+                        else => {
+                            state = .invalid;
+                        },
+                    }
+                },
+                .equal => {
+                    switch (c) {
+                        ' ', '\r', '\n', '\t' => {
+                            state = .expect_version;
+                            result.tag = .equal;
+                        },
+                        '=' => {
+                            state = .expect_version;
+                            result.tag = .equal;
+                        },
+                        'r', '0'...'9' => {
+                            state = .expect_version;
+                            result.tag = .equal;
+                            self.index -= 1; // backtrack
+                        },
+                        else => {
+                            state = .invalid;
+                        },
+                    }
+                },
+
+                // comes after an operator
+                .expect_version => {
+                    switch (c) {
+                        ' ', '\r', '\n', '\t' => {
+                            continue;
+                        },
+                        'r', '0'...'9' => {
+                            state = .version_literal;
+                            result.loc.start = self.index;
+                        },
+                        else => {
+                            result.tag = .invalid;
+                            break;
+                        },
+                    }
                 },
                 .newline => switch (c) {
                     ' ', '\t' => {
@@ -111,10 +275,11 @@ pub const Tokenizer = struct {
                     '\n' => {
                         // two newlines in a row end stanza
                         result.tag = .end_stanza;
-                        self.index = self.index + 1;
+                        self.index += 1;
                         break;
                     },
                     else => {
+                        result.tag = .end_field;
                         break;
                     },
                 },
@@ -141,7 +306,101 @@ pub const Tokenizer = struct {
                         else => continue,
                     }
                 },
-                .string_literal_backslash => {},
+                .string_literal_backslash => {
+                    switch (c) {
+                        0, '\n' => {
+                            result.tag = .invalid;
+                            break;
+                        },
+                        else => {
+                            state = .string_literal;
+                        },
+                    }
+                },
+                .version_literal => {
+                    switch (c) {
+                        'r' => {
+                            state = .version_literal_r;
+                        },
+                        '.' => {
+                            state = .version_literal_dot;
+                        },
+                        ')' => {
+                            break;
+                        },
+                        '\n' => {
+                            break;
+                        },
+                        ' ', '\r', '\t' => {
+                            self.index += 1;
+                            break;
+                        },
+                        '0'...'9' => continue,
+                        else => {
+                            result.tag = .invalid;
+                            break;
+                        },
+                    }
+                },
+                .version_literal_dot => {
+                    switch (c) {
+                        '0'...'9' => {
+                            state = .version_literal;
+                        },
+                        else => {
+                            result.tag = .invalid;
+                            break;
+                        },
+                    }
+                },
+                .version_literal_r => {
+                    switch (c) {
+                        '0'...'9' => {
+                            state = .version_literal_r_digit;
+                        },
+                        else => {
+                            result.tag = .invalid;
+                            break;
+                        },
+                    }
+                },
+                .version_literal_r_digit => {
+                    switch (c) {
+                        ' ', '\n', '\r', '\t' => {
+                            self.index += 1;
+                            break;
+                        },
+                        '0'...'9' => continue,
+                        else => {
+                            result.tag = .invalid;
+                            break;
+                        },
+                    }
+                },
+
+                .identifier => {
+                    switch (c) {
+                        'a'...'z', 'A'...'Z', '_', '0'...'9' => continue,
+                        '\n' => {
+                            break;
+                        },
+                        ' ', '\r', '\t' => {
+                            self.index += 1;
+                            break;
+                        },
+                        ':' => {
+                            break;
+                        },
+                        ',' => {
+                            break;
+                        },
+                        else => {
+                            result.tag = .invalid;
+                            break;
+                        },
+                    }
+                },
+
                 .invalid => switch (c) {
                     0 => if (self.index == self.buffer.len) {
                         result.tag = .invalid;
@@ -160,3 +419,57 @@ pub const Tokenizer = struct {
         return result;
     }
 };
+
+test "tokenize" {
+    try testTokenize("Field: val (<2)", &.{ .identifier, .colon, .identifier, .open_round, .less_than, .close_round, .eof });
+    try testTokenize("Field: val (<=2)", &.{ .identifier, .colon, .identifier, .open_round, .less_than_equal, .close_round, .eof });
+    try testTokenize("Field: val (> 2)", &.{ .identifier, .colon, .identifier, .open_round, .greater_than, .close_round, .eof });
+    try testTokenize("Field: val (>= 2)", &.{ .identifier, .colon, .identifier, .open_round, .greater_than_equal, .close_round, .eof });
+    try testTokenize("Field: val, val2", &.{ .identifier, .colon, .identifier, .comma, .identifier, .eof });
+}
+
+test "tokenize continuation" {
+    const data =
+        \\Field1: val1 val2
+        \\ continues
+        \\  again
+        \\Field2: val3
+    ;
+    try testTokenize(data, &.{ .identifier, .colon, .identifier, .identifier, .identifier, .identifier, .end_field, .identifier, .colon, .identifier, .eof });
+}
+
+test "tokenize string" {
+    const data = "\"string with \\\" inside";
+
+    try testTokenize(data, &.{ .string_literal, .eof });
+
+    var tokenizer = Tokenizer.init(data);
+    const token = tokenizer.next();
+    try testing.expectEqualStrings(data, token.lexeme(data).?);
+}
+
+test "tokenize version" {
+    const data = "(> 3.2.1)";
+    try testTokenize(data, &.{ .open_round, .greater_than, .close_round, .eof });
+
+    var tokenizer = Tokenizer.init(data);
+    _ = tokenizer.next();
+    const token = tokenizer.next();
+    try testing.expectEqualStrings("3.2.1", token.lexeme(data).?);
+}
+
+fn testTokenize(source: []const u8, expected_token_tags: []const Token.Tag) !void {
+    var tokenizer = Tokenizer.init(source);
+    for (expected_token_tags) |expected_token_tag| {
+        const token = tokenizer.next();
+        std.debug.print("{}: {?s}\n", .{ token, token.lexeme(source) });
+        try std.testing.expectEqual(expected_token_tag, token.tag);
+    }
+    // Last token should always be eof, even when the last token was invalid,
+    // in which case the tokenizer is in an invalid state, which can only be
+    // recovered by opinionated means outside the scope of this implementation.
+    const last_token = tokenizer.next();
+    try std.testing.expectEqual(Token.Tag.eof, last_token.tag);
+    try std.testing.expectEqual(source.len, last_token.loc.start);
+    try std.testing.expectEqual(source.len, last_token.loc.end);
+}
--- src/lib/root.zig
+++ src/lib/root.zig
@@ -6,5 +6,6 @@ test "library" {
     _ = @import("version.zig");
     _ = @import("RDescription.zig");
     _ = @import("packages.zig");
+    _ = @import("parse.zig");
     _ = @import("util.zig");
 }
