9b73285b52d72a57654b5b81d6944b91c8d25ffe
pomi601 <pomi601@aucklanduni.ac.nz>
2024-08-17 22:56:24 +1200

    remove original simple dcf parser

--- src/lib/root.zig
+++ src/lib/root.zig
@@ -2,7 +2,6 @@ const std = @import("std");
 const testing = std.testing;
 
 test "library" {
-    _ = @import("DebianControlFile.zig");
     _ = @import("version.zig");
     _ = @import("RDescription.zig");
     _ = @import("packages.zig");
