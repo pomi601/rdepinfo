9091eda1f4efd432b0432b0485df7a2a6a5d2d16
pomi601 <pomi601@aucklanduni.ac.nz>
2024-08-26 22:39:48 +1200

    move Repository.Index to separate file

--- src/lib/packages.zig
+++ src/lib/packages.zig
@@ -30,6 +30,8 @@ pub const Repository = struct {
     strings: ?StringStorage = null,
     packages: std.MultiArrayList(Package),
 
+    pub const Index = @import("repository_index.zig").Index;
+
     const Package = struct {
         name: []const u8 = "",
         version: Version = .{ .string = "" },
@@ -209,104 +211,6 @@ pub const Repository = struct {
             }
         }
     }
-
-    pub const Index = struct {
-        const MapType = std.StringHashMap(AvailableVersions);
-        items: MapType,
-
-        const AvailableVersions = union(enum) {
-            single: VersionIndex,
-            multiple: std.ArrayList(VersionIndex),
-
-            pub fn format(
-                self: AvailableVersions,
-                comptime fmt: []const u8,
-                options: std.fmt.FormatOptions,
-                writer: anytype,
-            ) !void {
-                _ = options;
-                _ = fmt;
-                switch (self) {
-                    .single => |vi| {
-                        try writer.print("(IndexVersion.single {s} {})", .{
-                            vi.version.string,
-                            vi.index,
-                        });
-                    },
-                    .multiple => |l| {
-                        try writer.print("(IndexVersion.multiple", .{});
-                        for (l.items) |x| {
-                            try writer.print(" {s}", .{x.version});
-                        }
-                        try writer.print(")", .{});
-                    },
-                }
-            }
-        };
-
-        const VersionIndex = struct { version: Version, index: usize };
-
-        /// Create an index of the repo. Caller must deinit with the
-        /// same allocator.
-        pub fn init(repo: Repository) !Index {
-            // Index only supports up to max Index.Size items.
-            if (repo.packages.len > std.math.maxInt(MapType.Size)) return error.OutOfMemory;
-            var out = MapType.init(repo.alloc);
-            try out.ensureTotalCapacity(@intCast(repo.packages.len));
-
-            const slice = repo.packages.slice();
-            const names = slice.items(.name);
-            const versions = slice.items(.version);
-
-            var idx: usize = 0;
-            while (idx < repo.packages.len) : (idx += 1) {
-                const name = names[idx];
-                const ver = versions[idx];
-
-                if (out.getPtr(name)) |p| {
-                    switch (p.*) {
-                        .single => |vi| {
-                            p.* = .{
-                                .multiple = std.ArrayList(VersionIndex).init(repo.alloc),
-                            };
-                            try p.multiple.append(vi);
-                            try p.multiple.append(.{
-                                .version = ver,
-                                .index = idx,
-                            });
-                        },
-                        .multiple => |*l| {
-                            try l.append(.{
-                                .version = ver,
-                                .index = idx,
-                            });
-                        },
-                    }
-                } else {
-                    out.putAssumeCapacityNoClobber(name, .{
-                        .single = .{
-                            .version = ver,
-                            .index = idx,
-                        },
-                    });
-                }
-            }
-            return .{ .items = out };
-        }
-
-        pub fn deinit(self: *Index) void {
-            var it = self.items.valueIterator();
-            while (it.next()) |v| switch (v.*) {
-                .single => continue,
-                .multiple => |l| {
-                    l.deinit();
-                },
-            };
-
-            self.items.deinit();
-            self.* = undefined;
-        }
-    };
 };
 
 test "PACKAGES.gz" {
@@ -325,7 +229,11 @@ test "PACKAGES.gz" {
     parser.parse(source.?) catch |err| switch (err) {
         error.ParseError => {
             if (parser.parse_error) |perr| {
-                std.debug.print("ERROR: ParseError: {s}: {}:{s}\n", .{ perr.message, perr.token, source.?[perr.token.loc.start..perr.token.loc.end] });
+                std.debug.print("ERROR: ParseError: {s}: {}:{s}\n", .{
+                    perr.message,
+                    perr.token,
+                    source.?[perr.token.loc.start..perr.token.loc.end],
+                });
             }
         },
         error.OutOfMemory => {
@@ -342,7 +250,10 @@ test "PACKAGES.gz" {
     var repo = try Repository.init(alloc);
     defer repo.deinit();
     try repo.read(source.?);
-    std.debug.print("Parse to Repository ({} packages) = {}ms\n", .{ repo.packages.len, @divFloor(timer.lap(), 1_000_000) });
+    std.debug.print(
+        "Parse to Repository ({} packages) = {}ms\n",
+        .{ repo.packages.len, @divFloor(timer.lap(), 1_000_000) },
+    );
 
     // after parser.parse() returns, we should be able to immediate
     // release the source. Note that repo.read() also uses it in this
---
+++ src/lib/repository_index.zig
@@ -0,0 +1,104 @@
+const std = @import("std");
+const Repository = @import("packages.zig").Repository;
+
+const version = @import("version.zig");
+const NameAndVersionConstraint = version.NameAndVersionConstraint;
+const Version = version.Version;
+
+pub const Index = struct {
+    const MapType = std.StringHashMap(AvailableVersions);
+    items: MapType,
+
+    const AvailableVersions = union(enum) {
+        single: VersionIndex,
+        multiple: std.ArrayList(VersionIndex),
+
+        pub fn format(
+            self: AvailableVersions,
+            comptime fmt: []const u8,
+            options: std.fmt.FormatOptions,
+            writer: anytype,
+        ) !void {
+            _ = options;
+            _ = fmt;
+            switch (self) {
+                .single => |vi| {
+                    try writer.print("(IndexVersion.single {s} {})", .{
+                        vi.version.string,
+                        vi.index,
+                    });
+                },
+                .multiple => |l| {
+                    try writer.print("(IndexVersion.multiple", .{});
+                    for (l.items) |x| {
+                        try writer.print(" {s}", .{x.version});
+                    }
+                    try writer.print(")", .{});
+                },
+            }
+        }
+    };
+
+    const VersionIndex = struct { version: Version, index: usize };
+
+    /// Create an index of the repo. Caller must deinit with the
+    /// same allocator.
+    pub fn init(repo: Repository) !Index {
+        // Index only supports up to max Index.Size items.
+        if (repo.packages.len > std.math.maxInt(MapType.Size)) return error.OutOfMemory;
+        var out = MapType.init(repo.alloc);
+        try out.ensureTotalCapacity(@intCast(repo.packages.len));
+
+        const slice = repo.packages.slice();
+        const names = slice.items(.name);
+        const versions = slice.items(.version);
+
+        var idx: usize = 0;
+        while (idx < repo.packages.len) : (idx += 1) {
+            const name = names[idx];
+            const ver = versions[idx];
+
+            if (out.getPtr(name)) |p| {
+                switch (p.*) {
+                    .single => |vi| {
+                        p.* = .{
+                            .multiple = std.ArrayList(VersionIndex).init(repo.alloc),
+                        };
+                        try p.multiple.append(vi);
+                        try p.multiple.append(.{
+                            .version = ver,
+                            .index = idx,
+                        });
+                    },
+                    .multiple => |*l| {
+                        try l.append(.{
+                            .version = ver,
+                            .index = idx,
+                        });
+                    },
+                }
+            } else {
+                out.putAssumeCapacityNoClobber(name, .{
+                    .single = .{
+                        .version = ver,
+                        .index = idx,
+                    },
+                });
+            }
+        }
+        return .{ .items = out };
+    }
+
+    pub fn deinit(self: *Index) void {
+        var it = self.items.valueIterator();
+        while (it.next()) |v| switch (v.*) {
+            .single => continue,
+            .multiple => |l| {
+                l.deinit();
+            },
+        };
+
+        self.items.deinit();
+        self.* = undefined;
+    }
+};
